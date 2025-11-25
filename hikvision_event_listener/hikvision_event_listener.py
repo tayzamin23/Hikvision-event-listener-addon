19ae6d4a9e8bcec06a5b86c97bdfaa8daa907d9c
Tayzar Min <tayzamin.pyay@gmail.com>
2025-11-25 14:22:26 +0630

    Update hikvision_event_listener.py

--- hikvision_event_listener/hikvision_event_listener.py
+++ hikvision_event_listener/hikvision_event_listener.py
@@ -6,7 +6,7 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 CAMERA_IP = "192.168.110.74"
 USERNAME = "admin"
 PASSWORD = "Diginet@2025"
-WEBHOOK_URL = "http://192.168.110.26:8123/api/webhook/hikvision_event"
+WEBHOOK_URL = "http://192.168.110.58:8123/api/webhook/hikvision_event"
 
 url = f"https://{CAMERA_IP}/ISAPI/Event/notification/alertStream"
 
@@ -54,3 +54,4 @@ while True:
     except Exception as e:
         print("Connection lost:", e)
         time.sleep(5)
+
